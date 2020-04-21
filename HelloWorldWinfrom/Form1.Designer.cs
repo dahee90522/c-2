c9498da365e69e0c0289ba441e036f250750456e
dahee90522 <s2018s40@e-mirim.hs.kr>
2020-04-21 14:20:36 +0900

    다시

--- HelloWorldWinfrom/Form1.Designer.cs
+++ HelloWorldWinfrom/Form1.Designer.cs
@@ -33,12 +33,14 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("굴림", 50F);
             this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(356, 197);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "yo";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
